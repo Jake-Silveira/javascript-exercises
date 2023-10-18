d134d2abcfc4090a649cf37daf63fb74625073dc
Jake-Silveira <Jacob1silveira@gmail.com>
2023-10-18 13:54:25 -0500

    passed without looking at solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, inputNumber) {
+    if (inputNumber >= 0){
+        let stringOutput = '';
+            for(let i = 0; i < inputNumber; i++){
+                stringOutput = stringOutput + inputString}
+        return stringOutput.toString()
+    } else return 'ERROR'
 };
 
 // Do not edit below this line
