91a3e745bc959b24b5235b9df612ab268787f7be
Jake-Silveira <Jacob1silveira@gmail.com>
2023-10-19 10:06:50 -0500

    complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    if(inputString != ""){
+        var outputString = "";
+            for(var i = inputString.length - 1; i >= 0; i--){
+                outputString = outputString + inputString[i]};
+                return outputString;
+            } else return ''
+    } 
+    
 
-};
+
 
 // Do not edit below this line
 module.exports = reverseString;
