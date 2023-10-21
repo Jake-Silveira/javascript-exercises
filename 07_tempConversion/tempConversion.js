e15b17a6314a3dd34b6fffd34f08d8c72cd4c2b3
Jake-Silveira <Jacob1silveira@gmail.com>
2023-10-21 09:58:25 -0500

    complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputTemp) {
+  var convertFormula = (inputTemp - 32) * (5 / 9);
+  let roundResult = parseFloat(convertFormula.toFixed(1));
+  return roundResult
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputTemp) {
+  var convertFormula = inputTemp * (9 / 5) + 32;
+  let roundResult = parseFloat(convertFormula.toFixed(1));
+  return roundResult
 };
 
 // Do not edit below this line
@@ -9,3 +15,6 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+//x °F ≘ (x − 32) × 5/9°C	      f to c
+//x °C ≘ (x × 9/5 + 32) °F      c to f
